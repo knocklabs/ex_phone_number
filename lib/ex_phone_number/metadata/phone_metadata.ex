00cd6b972e18314c3c1e0e8a2e51ae4819cd36ab
Aaron Tinio <aptinio@gmail.com>
2023-07-28 15:17:20 +0800

    Replace deprecated log level :warn (#70)

--- lib/ex_phone_number/metadata/phone_metadata.ex
+++ lib/ex_phone_number/metadata/phone_metadata.ex
@@ -70,7 +70,7 @@ defmodule ExPhoneNumber.Metadata.PhoneMetadata do
   alias ExPhoneNumber.Metadata.PhoneNumberDescription
 
   require Logger
-  Logger.configure(level: Application.compile_env(:ex_phone_number, :log_level, :warn))
+  Logger.configure(level: Application.compile_env(:ex_phone_number, :log_level, :warning))
 
   def from_xpath_node(xpath_node) do
     kwlist =
--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ExPhoneNumber.Mixfile do
       app: :ex_phone_number,
       version: @version,
       name: "ExPhoneNumber",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
